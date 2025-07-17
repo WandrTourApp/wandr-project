687800fbb5902395ea9258acb1d3964360619618
WandrTourApp <wandrtourapp@gmail.com>
2025-07-16 20:18:43 -0400

    Create App.js

---
+++ podcast-automation/frontend-interface/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import UploadProcessor from "./components/UploadProcessor";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      <UploadProcessor />
+    </div>
+  );
+}
+
+export default App;
